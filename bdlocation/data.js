cb824913996f36e2f6eacecd4e4eae37e2d5674a
Habibur Rahman <90517084+perfecthabibur@users.noreply.github.com>
2025-07-02 03:09:02 +0600

    Add files via upload

---
+++ bdlocation/data.js
@@ -0,0 +1,31 @@
+const locationData = {
+  "ঢাকা (Dhaka)": {
+    "ঢাকা জেলা (Dhaka District)": {
+      "সাভার (Savar)": {
+        "আমিনবাজার (Aminbazar)": {
+          "আমিনবাজার পোস্ট (Aminbazar Post)": [
+            "চকবাজার (Chakbazar)",
+            "শাহীবাগ (Shaheebag)"
+          ]
+        }
+      },
+      "কেরানীগঞ্জ (Keraniganj)": {
+        "আগানগর (Aganagar)": {
+          "আগানগর পোস্ট (Aganagar Post)": [
+            "আগানগর পূর্ব (East Aganagar)",
+            "আগানগর পশ্চিম (West Aganagar)"
+          ]
+        }
+      }
+    }
+  },
+  "চট্টগ্রাম (Chattogram)": {
+    "চট্টগ্রাম জেলা (Chattogram District)": {
+      "পটিয়া (Patiya)": {
+        "খরনা (Khorna)": {
+          "খরনা পোস্ট (Khorna Post)": ["খরনা গ্রাম (Khorna Village)"]
+        }
+      }
+    }
+  }
+};

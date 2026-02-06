d49384f0ef0ff28ab82637873b90bde62b92586e
Habibur Rahman <90517084+perfecthabibur@users.noreply.github.com>
2026-02-07 05:58:50 +0600

    Add ripple effect animation on button hover

--- js/main.js
+++ js/main.js
@@ -116,3 +116,24 @@ function ajax(method, url, data, success, error) {
     }
 
 //contact====
+//button animation=====
+document.querySelectorAll(".ripple-hover").forEach(el => {
+  el.addEventListener("mouseenter", function (e) {
+
+    const ripple = document.createElement("span");
+    ripple.className = "hover-ripple";
+
+    const rect = this.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+
+    ripple.style.width = ripple.style.height = size + "px";
+    ripple.style.left = (e.clientX - rect.left - size / 2) + "px";
+    ripple.style.top = (e.clientY - rect.top - size / 2) + "px";
+
+    this.appendChild(ripple);
+
+    setTimeout(() => {
+      ripple.remove();
+    }, 700);
+  });
+});
